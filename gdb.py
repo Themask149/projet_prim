0413d65e0a97247e5e8289d2a43cb5e70eb7f807
Themask <allandela@hotmail.fr>
2023-12-14 15:34:23 +0100

    Timing on miracl core done

--- gdb.py
+++ gdb.py
@@ -3,6 +3,10 @@
 import gdb
 import secrets
 import re
+import matplotlib.pyplot as plt
+import time 
+import json
+import numpy as np 
 
 cycle_tab=[]
 address_cyccnt="0xE0001004"
@@ -17,15 +21,18 @@ class MyCommand(gdb.Command):
         gdb.execute("mon reset halt")
         gdb.execute("break main")
         gdb.execute("continue")
+        gdb.execute("set pagination off")
+        gdb.execute("set python print-stack full")
         print("random_multiply init done")
 
     def invoke(self, arg, from_tty):
+        t = time.time()
         print("random_multiply invoke")
-        gdb.execute("b *main+8")
-        gdb.execute("b *main+58")
-        gdb.execute("b *multiply+24")
-        gdb.execute("b *multiply+28")
-        gdb.execute("continue")
+        gdb.execute("b *main+42")
+        gdb.execute("b 79")
+        #Ici on met les breakpoints pour la fonction que l'on veut tester
+        gdb.execute("b *ECP_C25519_clmul+270")
+        gdb.execute("b *ECP_C25519_clmul+274")
         print("Setting number of test \n")
         nbloop=re.findall(r'\d+', arg)
         if len(nbloop) == 0:
@@ -35,20 +42,47 @@ class MyCommand(gdb.Command):
         gdb.execute("set nbtest="+str(nbloop))
         gdb.execute("continue")
         gdb.execute("p/a nbtest")
-        for i in range(nbloop):
-            print("Test "+str(i)+"\n")
+
+        round=0
+        while nbloop>0:
+            print("Setting key for round "+str(round)+"\n")
             token=secrets.token_hex(32)
             for j in range(32):
-                gdb.execute("set (char) s["+str(j)+"]=0x"+token[2*j:2*j+2])
-            print("Token : "+token+"\n")
+                gdb.execute("set (char) key["+str(j)+"]=0x"+token[2*j:2*j+2])
+            token=secrets.token_hex(16)
+            for j in range(16):
+                gdb.execute("set (char) iv["+str(j)+"]=0x"+token[2*j:2*j+2])
             gdb.execute("continue")
-            gdb.execute("set *(int32_t*)("+address_cyccnt+")=0")
-            gdb.execute("continue")
-            res=gdb.parse_and_eval(address_cyccnt)
-            cycle_tab.append(res)
-            gdb.execute("continue")
-            print("Cycle count : "+str(res)+"\n")
-        print("C'est fini !\n")
-        print(cycle_tab)
+            for i in range(min(nbloop,100)):
+                print("")
+                gdb.execute("set *(int32_t*)("+address_cyccnt+")=0")
+                gdb.execute("continue")
+                res = int.from_bytes(gdb.selected_inferior().read_memory(int(address_cyccnt, 16), 4).tobytes(), 'little')
+                cycle_tab.append(int(res))
+                gdb.execute("continue")
+                print("Cycle count : "+str(res)+"\n")
+            nbloop-=min(nbloop,100)
+            round+=1
+        print("C'est fini en +"+str(time.time()-t)+"!\n")
+        save_results()
 
 MyCommand()
+
+def save_results():
+
+    with open("Timing_function.txt",'a') as f:
+        f.write("\n\nPour la fonction FP_F25519_inv :")
+        # json.dump(cycle_tab, f)
+        f.write("\nVoici les valeurs uniques ")
+        uniquetab=np.unique(cycle_tab)
+        np.savetxt(f, uniquetab, fmt='%d')
+        f.write("\nLe nombre de valeur unique est "+ str(len(uniquetab)))
+
+
+    # plt.hist(cycle_tab, bins=25)
+
+    # plt.xlabel('Nb of cycles')
+    # plt.ylabel('Frequency')
+    # plt.title('Histogram for multiply function')
+
+    # plt.savefig('histogram.png')
--- test.c
+++ test.c
@@ -1,34 +1,83 @@
 #include "core/c_arm/ecp_C25519.h"
 #include "core/c_arm/big_256_29.h"
+#include "core/c_arm/core.h"
 
 extern void init_SYSCLK();
 extern void init_Cortex();
 
-ECP_C25519 multiply(char s[32]){
-    ECP_C25519 G;
-    BIG_256_29 r;
-    BIG_256_29_fromBytesLen(r, s,32);
-    ECP_C25519_generator(&G);
-    ECP_C25519_mul(&G, r);
-    return G;
-}
+#define KK 32
 
-int main() {
-    volatile int nbtest=1;
-    init_SYSCLK();
-    init_Cortex();
-    char s[32]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+volatile int nbtest=1;
+core_aes a;    
+char key[32]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+                         0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+                         0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+                         0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+char iv[16]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+                         0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+char s[32]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                          0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                          0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                          0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x02};
-    for (int i=0; i<nbtest; i++){
-        ECP_C25519 res = multiply(s);
-        BIG_256_29 res_in_big;
-        ECP_C25519_get(res_in_big, &res);
-        int dumb = BIG_256_29_comp(res_in_big, res_in_big);
-        if (dumb){
-            return 1;
-        }
+ECP_C25519 res;
+BIG_256_29 res_in_big;
+
+void multiply(char s[32], ECP_C25519 *res){
+    BIG_256_29 r;
+    BIG_256_29_fromBytesLen(r, s,32);
+    ECP_C25519_generator(res);
+    // put Syscnt to 0 ARRET
+    ECP_C25519_mul(res, r);
+    // gather Syscnt ARRET
+}
+void clamping(char key[32]){
+    key[31]&=0xF8;
+    key[0]&=0x7F;
+    key[0]|=0x40;
+}
+
+void random_aes(core_aes *a, char random[32]){
+    // on veut à partir d'une clé et d'un iv généré par python
+    // clé 32 bytes mais on veut la sortie de AES = 32 bytes
+    AES_encrypt(a,random);
+    AES_encrypt(a,random+16);
+    clamping(random);
+}
+
+int min(int a, int b){
+    if (a<b){
+        return a;
     }
+    return b;
+}
+
+
+int main() {
+    //init nbround here
+    init_SYSCLK();
+    init_Cortex();
+    while (nbtest>0){
+        //here init key iv at random with python ARRET
+        AES_init(&a,CTR16,KK,key,iv);
+        for (int i=0; i<min(nbtest,100); i++){
+            random_aes(&a,s);
+            ECP_C25519 res;
+            multiply(s,&res);
+            BIG_256_29 res_in_big;
+            ECP_C25519_get(res_in_big, &res);
+            BIG_256_29_toBytes(s, res_in_big);     
+        }
+        nbtest=nbtest-min(nbtest,100);
+        AES_end(&a);
+    }
+
+    // for (int i=0; i<nbtest; i++){
+    //     multiply(s,&res);
+    //     ECP_C25519_get(res_in_big, &res);
+    //     BIG_256_29_toBytes(s, res_in_big);
+    // }
     return 0;
 }
+
+
+
