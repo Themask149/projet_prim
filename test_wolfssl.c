decebf20ae2230192a245daa04eed6356db556ec
Themask <allandela@hotmail.fr>
2024-01-02 17:37:37 +0100

    multiplication wolfssl to fix

--- test_wolfssl.c
+++ test_wolfssl.c
@@ -1,25 +1,31 @@
 #include "wolfssl/wolfssl/options.h"
 #include "wolfssl/wolfssl/wolfcrypt/settings.h"
 #include "wolfssl/wolfssl/wolfcrypt/curve25519.h"
+#define CURVE25519_KEYSIZE 32
+
+static const word32 kCurve25519BasePoint[CURVE25519_KEYSIZE/sizeof(word32)] = {
+#ifdef BIG_ENDIAN_ORDER
+    0x09000000
+#else
+    9
+#endif
+};
 
 int main(void){
     int ret;
-    curve25519_key key;
     byte in[32] = {0};
     byte out[32];
 
-    ret = wc_curve25519_init(&key);
-    if (ret != 0) {
-        printf("wc_curve25519_init failed with error %d\n", ret);
-        return ret;
-    }
-    in[0] = 1;
-    ret = wc_curve25519_mul(in,32,out,&key);
-    if (ret != 0) {
-        printf("wc_curve25519_mul failed with error %d\n", ret);
-        wc_curve25519_free(&key);
-        return ret;
-    }
+    in[31]=1;
+    fe_init();
 
-    wc_curve25519_free(&key);
+    SAVE_VECTOR_REGISTERS(return _svr_ret;);
+
+    ret = curve25519(out, in, (byte*)kCurve25519BasePoint);
+
+    RESTORE_VECTOR_REGISTERS();
+    for (;;){
+
+    }
+    return ret;
 }
