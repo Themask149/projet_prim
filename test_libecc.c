ba5602d407db8aad3bd5a38970f018959a3cdafe
Themask <allandela@hotmail.fr>
2024-01-18 17:22:11 +0100

    to fixed: libecc with special functions inside

--- test_libecc.c
+++ test_libecc.c
@@ -8,20 +8,31 @@ int main(void){
     init_Cortex();
     const ec_str_params *ec_str_params;
     ec_params curve_params;
-    // nn_t private_key = NULL;
+    nn private_key;
     ec_get_curve_params_by_type(WEI25519 ,&ec_str_params);
     int ret = import_params(&curve_params, ec_str_params);
     if (ret) {
         for(;;){}
         return -1;
     }
-    // nn_init(private_key,32);
-    // nn_set_word_value(private_key,2);
-    // prj_pt Q;
-    // ret = prj_pt_init(&Q, &(curve_params.ec_curve));
-    // ret = prj_pt_mul(&Q, private_key,&(curve_params.ec_gen));
-    // aff_pt_t out;
-    // prj_pt_to_aff_pt(&out, &Q, &(curve_params.ec_curve));
+    nn_init(&private_key,32);
+    nn_set_word_value(&private_key,2);
+    prj_pt Q;
+    ret = prj_pt_init(&Q, &(curve_params.ec_curve));
+    if (ret) {
+        for(;;){}
+        return -1;
+    }
+    ret = prj_pt_mul(&Q, &private_key,&(curve_params.ec_gen));
+    if (ret) {
+        for(;;){}
+        return -1;
+    }
+    aff_pt out;
+
+    prj_pt_to_aff(&out, &Q);
+
+    for (;;){}
     return 0;
 
     
